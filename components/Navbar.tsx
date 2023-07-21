52490425dbbf7161e94ddf716d884074b5ce44ae
kamilstalinski <kamstal95@gmail.com>
2023-07-22 01:08:30 +0200

    logo test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,16 +19,16 @@ const Navbar = () => {
     <nav
       className={`${lato.className} container mx-auto h-32 w-full flex justify-between items-center px-[4%] pb-4`}>
       <div className='nav__logo h-full flex items-end md:items-center relative'>
-        <Link href='/'>
-          {/* <div className='image__container relative w-[210px] h-[83px] md:w-[122px] md:h-[48px] z-[100]'>
+        {/* <Link href='/'>
+          <div className='image__container relative w-[210px] h-[83px] md:w-[122px] md:h-[48px] z-[100]'>
             <Image src='/Dobre_precle_logo.svg' alt='Dobre Precle logo' fill />
-          </div> */}
-          <img
-            className='w-1/3'
-            src='/Dobre_precle_logo.svg'
-            alt='Dobre Precle logo'
-          />
-        </Link>
+          </div>
+        </Link> */}
+        <img
+          className='w-1/3'
+          src='/Dobre_precle_logo.svg'
+          alt='Dobre Precle logo'
+        />
       </div>
       <div className='nav__links h-full'>
         <ul className='h-full flex gap-11 lg:gap-6 items-end text-[#971C25] font-bold'>
