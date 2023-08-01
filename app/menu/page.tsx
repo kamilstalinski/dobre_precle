ee6de4d2022f5679f27fd9b403113cf8032ae9af
kamilstalinski <kamstal95@gmail.com>
2023-08-02 01:07:07 +0200

    added menu desktop page

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,130 @@
-import React from "react";
+import Image from "next/image";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <section>
+      <div className='background background before:bg-[url("/kanapki_menu.png")]  w-full h-[27rem] md:h-[15rem] flex items-center justify-center'>
+        <h1 className='text-h1 lg:text-h1-tablet md:text-h1-mobile text-white text-center font-bold leading-tight'>
+          Menu
+        </h1>
+      </div>
+      <div className='container mx-auto px-[4%]'>
+        <div className='container mx-auto px-[4%] w-4/5 my-[30px] md:my-[20px] p-[30px] text-center text-white bg-[#EB8C11] rounded-3xl'>
+          <h2 className='text-h3 md:text-h3-mobile font-bold mb-4'>
+            DOBRE PRECLE – Polecają się na przekąskę!
+          </h2>
+          <p className='text-p md:text-p-mobile'>
+            Oto on, nasz{" "}
+            <span className='font-bold'>król przekąsek, DOBRY PRECEL!</span>{" "}
+            Dobre Precle wypiekamy na miejscu, serwując Ci zawsze świeże precle
+            podawane samodzielnie, a także w formie smacznych autorskich kanapek
+            na zimno lub w wersji zapiekanej. Na każdą okazję, zarówno na mały,
+            jak i duży głód, znajdziemy dla Ciebie odpowiedniego precla.
+          </p>
+        </div>
+      </div>
+      <div className='relative w-full h-[1000px] flex flex-col justify-between items-center py-[200px] overflow-hidden'>
+        <p className='w-[30%] text-center'>
+          Przygotowaliśmy szeroki wybór, który obejmuje{" "}
+          <span className='font-bold'>
+            precle suche, z różnorodnymi posypkami
+          </span>{" "}
+          oraz <span className='font-bold'>wyjątkowe precle na słodko,</span>{" "}
+          które są prawdziwą ucztą dla podniebienia.
+        </p>
+        <p className='w-[30%] text-center mb-[140px]'>
+          A jeśli szukasz czegoś bardziej sycącego, np. do pracy czy w podróży,
+          to nasze{" "}
+          <span className='font-bold'>
+            precle w autorskiej wersji kanapkowej
+          </span>{" "}
+          będą idealnym rozwiązaniem! Precle autorskie podnoszą standard
+          tradycyjnych przekąsek do wyższego poziomu, odpowiadając na różne
+          gusta smakowe.
+        </p>
+        <div className='w-[30%] h-[40%] absolute top-0 left-0'>
+          <Image
+            src='/precle_suche.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+            style={{
+              rotate: "-10deg",
+              scale: 1.3,
+              translate: "-15% 0",
+            }}
+          />
+        </div>
+        <div className='w-[20%] h-[40%] absolute top-[20%] left-[18%]'>
+          <Image
+            src='/precle_suche_sol.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+          />
+        </div>
+        <div className='w-[35%] h-[60%] absolute top-[34%] left-0'>
+          <Image
+            src='/kanapka_1.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+            objectPosition='left'
+          />
+        </div>
+        <div className='w-[25%] h-[60%] absolute top-[54%] left-[5%]'>
+          <Image
+            src='/kanapka_2.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+            objectPosition='left'
+          />
+        </div>
+        <div className='w-[25%] h-[60%] absolute top-[58%] left-[24%]'>
+          <Image
+            src='/kanapka_3.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+            objectPosition='left'
+          />
+        </div>
+        <div className='w-[28%] h-[60%] absolute top-[55%] left-[43%]'>
+          <Image
+            src='/kanapka_4.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+            objectPosition='left'
+          />
+        </div>
+        <div className='w-[40%] h-[60%] absolute top-[50%] right-0'>
+          <Image
+            src='/kanapka_5.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+            objectPosition='right'
+          />
+        </div>
+        <div className='w-[30%] h-[60%] absolute top-[23%] right-0'>
+          <Image
+            src='/kanapka_6.png'
+            alt='precle suche'
+            fill
+            objectFit='contain'
+            objectPosition='right'
+          />
+        </div>
+      </div>
+      <div className='background background before:bg-[url("/kanapki_menu_2.png")] w-full h-[27rem] md:h-[15rem] flex items-center justify-center'>
+        <h1 className='text-[60px] md:text-[30px] text-white text-center font-bold leading-tight container'>
+          Wybierz swoje ulubione precle i ciesz się ich smakiem!
+        </h1>
+      </div>
+    </section>
+  );
 };
 
 export default Menu;
